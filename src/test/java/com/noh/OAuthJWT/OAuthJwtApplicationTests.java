3f3c6de428f4d29efe1956acea605ca62cb74efe
Noh Hyeon Jin <hjforj@naver.com>
2022-05-31 13:21:36 +0900

    initial commit

---
+++ src/test/java/com/noh/OAuthJWT/OAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noh.OAuthJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
